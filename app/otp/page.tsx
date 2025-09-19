d02458e877af8ab2662a6e7e194624d664d43ae8
Ochweda <curtis.oluoch@gmail.com>
2025-09-19 17:21:25 +0300

    Arrow and logo spacing in onboarding pages

--- app/otp/page.tsx
+++ app/otp/page.tsx
@@ -57,7 +57,7 @@ export default function VerifyEmail() {
 
         {/* Form Section */}
         <div className="w-full lg:w-1/2 text-center lg:text-left flex flex-col ">
-          <div className="flex pt-10 items-center w-full justify-center gap-40 md:gap-30 md:px-6 md:pt-6 lg:pt-8 lg:gap-55 lg:pr-40">
+          <div className="flex pt-10 items-center w-full justify-center gap-30 md:gap-30 md:px-6 md:pt-6 lg:pt-8 lg:gap-55 lg:pr-40">
             <button 
               onClick={() => router.back()}
               className="cursor-pointer flex items-center mr-2 md:mr-4 text-[#2C2C2C] dark:text-[#FFFFFF] hover:text-gray-600 dark:hover:text-gray-300 transition-colors duration-200"
